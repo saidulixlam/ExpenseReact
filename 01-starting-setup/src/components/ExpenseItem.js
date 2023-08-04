77a6ed9f5f87bd160aba670872be10bc38576f24
saidulixlam <saidulixlam@gmail.com>
2023-08-04 22:16:47 +0530

    initial commit

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+const ExpenseItem = () => {
+    return (
+        <div>
+            <h3>Expense Items</h3>
+
+            <p>Food Rs 10</p>
+
+            <p>Petrol Rs 100</p>
+
+            <p>Movies Rs 200</p>
+        </div>
+    );
+}
+
+export default ExpenseItem;

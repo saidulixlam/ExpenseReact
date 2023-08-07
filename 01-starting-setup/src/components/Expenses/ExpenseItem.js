a043dee67fceb85ed73d141381544e32ad488b54
saidulixlam <saidulixlam@gmail.com>
2023-08-07 13:36:06 +0530

    Update expense with expense button

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -2,19 +2,19 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+import React,{useState} from 'react';
 const ExpenseItem = (props) => {
-    // const expDate= new Date(2021, 2, 2).toISOString();
-    // const expPrice=234.90;
-    // const expTitle='Car Insurance';
-    // const location = 'Location';
+    const title=props.expense.title;
+   const [price,setPrice]=useState(props.expense.amount);
     function clickFunction() {
-        console.log('clicked');
+        setPrice('100');
+        console.log(price);
     }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.expense.date} />
-            <ExpenseDetails amount={props.expense.amount} location={props.expense.location} title={props.expense.title}/>
-            <button onClick={clickFunction}>Delete Expense</button>
+            <ExpenseDetails amount={price} location={props.expense.location} title={title}/>
+            <button onClick={clickFunction}>Update Expense</button>
         </Card>
     );
 }
